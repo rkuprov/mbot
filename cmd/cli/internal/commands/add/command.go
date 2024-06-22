616be654eea950effff26cba5c0e4dad41fd354f
Roman Kuprov <roman@getbanyan.com>
2024-06-22 17:01:54 -0400

    client: add alias to Subscription subcommand

--- cmd/cli/internal/commands/add/command.go
+++ cmd/cli/internal/commands/add/command.go
@@ -2,5 +2,5 @@ package add
 
 type Cmd struct {
 	Customer     Customer     `cmd:"" help:"Add a customer"`
-	Subscription Subscription `cmd:"" help:"Add a subscription"`
+	Subscription Subscription `cmd:"" aliases:"sub" help:"Add a subscription"`
 }
