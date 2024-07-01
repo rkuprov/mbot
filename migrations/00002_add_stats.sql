53a3cd3df6c589963a1deed3025f9965ae6420c5
Roman Kuprov <roman@banyan.com>
2024-07-01 13:46:14 -0400

    add stats migration

---
+++ migrations/00002_add_stats.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS stats
+(
+    customer_id INT NOT NULL,
+    subscription_id VARCHAR(36) NOT NULL references subscriptions (id),
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX stats_customer_id_idx ON stats (customer_id);
+CREATE INDEX stats_subscription_id_idx ON stats (subscription_id);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP INDEX IF EXISTS stats_customer_id_idx;
+DROP INDEX IF EXISTS stats_subscription_id_idx;
+DROP TABLE IF EXISTS stats;
+-- +goose StatementEnd
