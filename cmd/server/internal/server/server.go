a5e7e6ad9682dabdae1f172b86622463f1b34d6a
Roman Kuprov <roman@banyan.com>
2024-07-04 16:54:26 -0400

    update login proto add Login method to mbot service

--- cmd/server/internal/server/server.go
+++ cmd/server/internal/server/server.go
@@ -1,18 +1,21 @@
 package server
 
 import (
+	"github.com/rkuprov/mbot/pkg/auth"
 	"github.com/rkuprov/mbot/pkg/gen/mbotpb/mbotpbconnect"
 	"github.com/rkuprov/mbot/pkg/store"
 )
 
 type MBot struct {
-	db *store.Store
+	db   *store.Store
+	auth *auth.Auth
 }
 
 var _ mbotpbconnect.MBotServerServiceHandler = (*MBot)(nil)
 
-func NewMBot(db *store.Store) *MBot {
+func NewMBot(db *store.Store, auth *auth.Auth) *MBot {
 	return &MBot{
-		db: db,
+		db:   db,
+		auth: auth,
 	}
 }
--- cmd/server/routes.go
+++ cmd/server/routes.go
@@ -5,14 +5,25 @@ import (
 	"net/http"
 
 	"github.com/rkuprov/mbot/cmd/server/internal/server"
+	"github.com/rkuprov/mbot/pkg/auth"
+	"github.com/rkuprov/mbot/pkg/cfg"
 	"github.com/rkuprov/mbot/pkg/gen/mbotpb/mbotpbconnect"
 	"github.com/rkuprov/mbot/pkg/handlers"
 	"github.com/rkuprov/mbot/pkg/store"
 )
 
-func SetupRoutes(mux *http.ServeMux, db *store.Store) {
+func SetupRoutes(mux *http.ServeMux, configs *cfg.Cfg) {
 	ctx := context.Background()
-	m := server.NewMBot(db)
+
+	db, err := store.New(configs.Postgres)
+	if err != nil {
+		panic(err)
+	}
+	a, err := auth.New(configs.Postgres)
+	if err != nil {
+		panic(err)
+	}
+	m := server.NewMBot(db, a)
 
 	// grpc-connect
 	path, handler := mbotpbconnect.NewMBotServerServiceHandler(m)
